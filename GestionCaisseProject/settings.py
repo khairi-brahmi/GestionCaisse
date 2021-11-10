6031c4d170248888f0fe42f6617cf4b72b57ee66
khairi-brahmi <khairibrahmi00@gmail.com>
2021-11-10 09:20:10 -0600

    create product/category/discounts models and views

--- GestionCaisseProject/settings.py
+++ GestionCaisseProject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_swagger',
-    'backendapis',
+    'backendapis', 
     'cartsapis',
     'django.contrib.sites',
     'allauth',
